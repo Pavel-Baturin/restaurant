34c822503649589ff3d83b97c7cca4f7a8122259
Pavel-Baturin <pavel.baturin.92@gmail.com>
2022-09-07 14:50:09 +0300

    Adds header, hero, advantages, order and history.

---
+++ src/index.js
@@ -0,0 +1,8 @@
+import './sass/index.scss';
+
+$(document).ready(function () {
+  $('.header__burger').click(function () {
+    $('.header__burger, .header__nav').toggleClass("active");
+    $('body').toggleClass("lock");
+  });
+});
