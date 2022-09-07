26305fe6d9795ef45c3adf96bf7a58e24518a98f
Pavel-Baturin <86241552+Pavel-Baturin@users.noreply.github.com>
2022-09-07 11:51:03 +0000

    Deploying to gh-pages from @ Pavel-Baturin/restaurant@34c822503649589ff3d83b97c7cca4f7a8122259 🚀

---
+++ index.53ad94ec.js
@@ -0,0 +1,2 @@
+$(document).ready((function(){$(".header__burger").click((function(){$(".header__burger, .header__nav").toggleClass("active"),$("body").toggleClass("lock")}))}));
+//# sourceMappingURL=index.53ad94ec.js.map
---
+++ index.98e6480b.js
@@ -0,0 +1,2 @@
+$(document).ready((function(){$(".header__burger").click((function(){$(".header__burger, .header__nav").toggleClass("active"),$("body").toggleClass("lock")}))}));
+//# sourceMappingURL=index.98e6480b.js.map
