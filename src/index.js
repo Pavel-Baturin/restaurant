4ee01c0ba8cb7f515a8d2fb626779adf53388a5a
Pavel-Baturin <pavel.baturin.92@gmail.com>
2022-09-14 17:55:04 +0300

    adds section footer

--- src/index.js
+++ src/index.js
@@ -5,4 +5,8 @@ $(document).ready(function () {
     $('.header__burger, .header__nav').toggleClass("active");
     $('body').toggleClass("lock");
   });
+  window.matchMedia('(min-width: 768px)').change(function (e) {
+    if (!e.matches) return;
+    $('.header__burger, .header__nav').toggleClass('active');
+  });
 });
